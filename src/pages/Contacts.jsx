4b1441420d3fdb200ae09c66648a17b9012e0028
shashank <shashankhosamani1109@gmail.com>
2024-09-13 15:46:04 +0530

    contacs up

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -32,9 +32,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Shashank.L.Hosamani",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "shashankhosamani1109@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
